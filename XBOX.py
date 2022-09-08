01152fb54afd5aea9a8631f90ce1378763d9ed29
larrythegnome <logankeaton5@gmail.com>
2022-09-08 13:15:49 -0400

    up

--- XBOX.py
+++ XBOX.py
@@ -1,5 +1,19 @@
+import time
+import os
 
-print("yo mama so stupied, she got tickets to...")
+joke = ["Yo mama", "is so stupied", "she got tickets to", ".", ".", "."]
+counter = 0
+total_joke = len(joke)
+print(total_joke)
+def xbox():
+    global counter
+    while counter < total_joke:
+        print(joke[counter])
+        time.sleep(.1)
+        counter = counter + 1
+    counter = 0
+    xbox()
+xbox()
 
 print(" -------     -------     -----------       ----------     -------     ------- ")
 print(" \      \   /      /     |         |      /          \    \      \   /      / ")
@@ -11,11 +25,3 @@ print("   /             \       |        \     \    \    /    /    /            
 print("  /      / \      \      |         |     \    ----    /    /      / \      \  ")
 print(" /      /   \      \     |         |      \          /    /      /   \      \ ")
 print(" -------     -------     -----------       ----------     -------     ------- ")
-
-
-
-
-
-
-
-
