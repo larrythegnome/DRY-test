e263881eccd5da8ebafca7473ffa4eef9107042f
larrythegnome <logankeaton5@gmail.com>
2022-09-09 12:12:48 -0400

    live

--- XBOX.py
+++ XBOX.py
@@ -1,19 +1,4 @@
-import time
-import os
 
-joke = ["Yo mama", "is so stupied", "she got tickets to", ".", ".", "."]
-counter = 0
-total_joke = len(joke)
-print(total_joke)
-def xbox():
-    global counter
-    while counter < total_joke:
-        print(joke[counter])
-        time.sleep(.1)
-        counter = counter + 1
-    counter = 0
-    xbox()
-xbox()
 
 print(" -------     -------     -----------       ----------     -------     ------- ")
 print(" \      \   /      /     |         |      /          \    \      \   /      / ")
@@ -25,3 +10,14 @@ print("   /             \       |        \     \    \    /    /    /            
 print("  /      / \      \      |         |     \    ----    /    /      / \      \  ")
 print(" /      /   \      \     |         |      \          /    /      /   \      \ ")
 print(" -------     -------     -----------       ----------     -------     ------- ")
+print("         -----         -----    -------        -------    ----------          ")
+print("         |   |         |   |    \      \      /      /    |        |          ")
+print("         |   |         |   |     \      \    /      /     |    -----          ")
+print("         |   |         |   |      \      \  /      /      |   |               ")
+print("         |   |         |   |       \      \/      /       |    -----          ")
+print("         |   |         |   |        \            /        |        |          ")
+print("         |   |         |   |         \          /         |    -----          ")
+print("         |   |         |   |          \        /          |    |              ")
+print("         |   --------  |   |           \      /           |    -----          ")
+print("         |          |  |   |            \    /            |        |          ")
+print("         ------------  -----             ----             ----------          ")
